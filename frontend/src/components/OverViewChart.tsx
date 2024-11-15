fb251aef21b73acafacd36a0a32bf0629901ffec
Christian <111076892+i-christian@users.noreply.github.com>
2024-11-15 20:48:02 +0200

    feat: improve the overview chart to include both expenses and income line graphs

--- frontend/src/components/OverViewChart.tsx
+++ frontend/src/components/OverViewChart.tsx
@@ -9,9 +9,16 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
+import { Component, onMount } from 'solid-js';
 
-function OverViewChart() {
-  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+const OverViewChart: Component = () => {
+  onMount(() => {
+    ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+  });
+
+  // let months = [
+  //   "January", "Feb"
+  // ]
 
   const options = {
     responsive: true,
@@ -33,26 +40,37 @@ function OverViewChart() {
     },
   };
 
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
 
   const chartdata = {
     labels,
     datasets: [
       {
-        label: 'Expenses - Last 7 Months',
-        data: [100, 300, 200, 100, 200, 400, 300],
+        label: 'Expenses - Last 6 Months',
+        data: [100, 300, 200, 100, 200, 400],
         borderColor: '#B51021',
         backgroundColor: '#B51021',
         color: '#B51021',
         tension: 0.3,
       },
+
+      {
+        label: 'Income - Last 6 Months',
+        data: [50, 100, 250, 50, 200, 300],
+        borderColor: '#070ab5',
+        backgroundColor: '#070ab5',
+        color: '#070ab5',
+        tension: 0.3,
+      },
+
     ],
+
   };
 
   return (
-    <div class="w-full h-96 shadow-md p-6 mb-10 rounded-md bg-white">
+    <section class="w-full h-96 shadow-md p-6 mb-10 rounded-md bg-white">
       <Line options={options} data={chartdata} />
-    </div>
+    </section>
   );
 }
 
