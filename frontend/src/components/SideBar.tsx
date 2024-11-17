594dd92dd771afb9b2e51ad5c7bb1328b4db7861
christian <111076892+i-christian@users.noreply.github.com>
2024-11-17 23:41:05 +0200

    add a new menu on nav section, a menu to go back to the homepage of the application.

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,18 +1,32 @@
-import { Accessor, Component, Match, Setter, Suspense, Switch, createResource } from "solid-js"
+import {
+  Accessor,
+  Component,
+  Match,
+  Setter,
+  Suspense,
+  Switch,
+  createResource,
+} from "solid-js";
 import { A, useNavigate } from "@solidjs/router";
 import { setIsLoggedIn } from "../index";
 import { getUser } from "../hooks/useFetch";
 
-const SideBar: Component<{ open: Accessor<boolean>, setOpen: Setter<boolean> }> = (props) => {
+const SideBar: Component<{
+  open: Accessor<boolean>;
+  setOpen: Setter<boolean>;
+}> = (props) => {
   const navigate = useNavigate();
-  const [user] = createResource(getUser)
+  const [user] = createResource(getUser);
 
   const logout = async () => {
     try {
-      const response = await fetch(`//${window.location.host}/api/auth/logout`, {
-        method: "GET",
-        credentials: "include",
-      });
+      const response = await fetch(
+        `//${window.location.host}/api/auth/logout`,
+        {
+          method: "GET",
+          credentials: "include",
+        },
+      );
 
       if (response.status === 500) {
         console.log("Internal server error");
@@ -20,16 +34,15 @@ const SideBar: Component<{ open: Accessor<boolean>, setOpen: Setter<boolean> }> 
         setIsLoggedIn(false);
         localStorage.removeItem("isLoggedIn");
         navigate("/login");
-
       }
-
     } catch (err) {
       console.error("Error logging out:", err);
     }
   };
 
   return (
-    <aside class={`bg-emerald-900 fixed left-0 top-0 shadow-3xl p-5 h-screen ${props.open() ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 transition-transform duration-500 px-4 w-64 z-50 flex flex-col`}
+    <aside
+      class={`bg-emerald-900 fixed left-0 top-0 shadow-3xl p-5 h-screen ${props.open() ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 transition-transform duration-500 px-4 w-64 z-50 flex flex-col`}
     >
       <header class="flex justify-end lg:hidden">
         <button
@@ -37,11 +50,20 @@ const SideBar: Component<{ open: Accessor<boolean>, setOpen: Setter<boolean> }> 
           onClick={() => props.setOpen(!props.open())}
           aria-label="Close Sidebar"
         >
-
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
           </svg>
-
         </button>
       </header>
 
@@ -60,21 +82,37 @@ const SideBar: Component<{ open: Accessor<boolean>, setOpen: Setter<boolean> }> 
       </h2>
       <hr class="my-5" />
       <nav class="flex flex-col bg-emerald-100 shadow-inner rounded-lg px-5 py-5 gap-2">
-        <A href="/budget" class="link">Budget </A>
-        <A href="/transactions" class="link">Transactions </A>
-        <A href="/income" class="link">Income </A>
-        <A href="/expenses" class="link">Expenses </A>
-        <A href="/reports" class="link">Reports </A>
-        <A href="/settings" class="link">Settings </A>
+        <A href="/" class="link">
+          Home{" "}
+        </A>
+        <A href="/budget" class="link">
+          Budget{" "}
+        </A>
+        <A href="/transactions" class="link">
+          Transactions{" "}
+        </A>
+        <A href="/income" class="link">
+          Income{" "}
+        </A>
+        <A href="/expenses" class="link">
+          Expenses{" "}
+        </A>
+        <A href="/reports" class="link">
+          Reports{" "}
+        </A>
+        <A href="/settings" class="link">
+          Settings{" "}
+        </A>
       </nav>
       <hr class="my-5 botton-0" />
-      <button onClick={logout} class="py-2 px-5 bg-red-500 text-white hover:bg-red-900 rounded-lg">
+      <button
+        onClick={logout}
+        class="py-2 px-5 bg-red-500 text-white hover:bg-red-900 rounded-lg"
+      >
         Logout
       </button>
-
     </aside>
-
-  )
-}
+  );
+};
 
 export default SideBar;
