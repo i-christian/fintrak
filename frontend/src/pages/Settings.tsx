01842de160406bcef05fd5b5008865ba753b8b25
christian <111076892+i-christian@users.noreply.github.com>
2024-11-18 00:09:25 +0200

    style: add a delete account section on setting page

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,16 +1,19 @@
 import { Component, createSignal } from "solid-js";
 
 const Settings: Component = () => {
-  const [open, setOpen] = createSignal<boolean>(false);
+  const [openEdit, setOpenEdit] = createSignal<boolean>(false);
+  const [openDelete, setOpenDelete] = createSignal<boolean>(false);
 
   return (
-    <main class="mt-10 flex flex-col gap-5">
-      <div class="shadow-2xl border p-5 rounded-2xl">
+    <main class="mt-10 flex flex-col md:flex-row gap-5">
+      <div class="shadow-2xl border grow p-5 rounded-2xl ">
         <section>
           <p class="font-bold text-xl">Change Name </p>
-          <div class={`flex flex-row gap-5 ${open() ? "hidden" : "block"} `}>
+          <div
+            class={`flex flex-row gap-5 ${openEdit() ? "hidden" : "block"} `}
+          >
             <span class="text-xl py-2">User1</span>
-            <button onClick={() => setOpen(!open())}>
+            <button onClick={() => setOpenEdit(!openEdit())}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -27,20 +30,22 @@ const Settings: Component = () => {
               </svg>
             </button>
           </div>
-          <form class={`flex flex-row gap-5 ${!open() ? "hidden" : "block"} `}>
+          <form
+            class={`flex flex-row gap-5 ${!openEdit() ? "hidden" : "block"} `}
+          >
             <label for="name">
               New Name:
               <input id="name" class="ml-2 p-1 rounded-md border" type="text" />
             </label>
             <button
               class="bg-emerald-600 hover:bg-emerald-900 rounded-md px-5 text-white w-fit"
-              onClick={() => setOpen(!open())}
+              onClick={() => setOpenEdit(!openEdit())}
             >
               Save
             </button>
             <button
               class="bg-red-600 hover:bg-red-900 rounded-md px-5 text-white w-fit"
-              onClick={() => setOpen(!open())}
+              onClick={() => setOpenEdit(!openEdit())}
             >
               cancel
             </button>
@@ -79,7 +84,47 @@ const Settings: Component = () => {
           </form>
         </section>
       </div>
-      <section>Delete Account</section>
+      <section class="shadow-2xl border grow p-5 rounded-2xl">
+        <h1 class="font-bold text-xl text-red-900"> Account Deactivation </h1>
+        <div
+          class={`flex flex-row gap-5 ${openDelete() ? "hidden" : "block"} `}
+        >
+          <span class="text-xl py-2">User1</span>
+          <button onClick={() => setOpenDelete(!openDelete())}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          class={`flex flex-row gap-5 ${!openDelete() ? "hidden" : "block"} `}
+        >
+          <h1 class="italic">Are you sure you want to delete?</h1>
+          <button
+            class="bg-emerald-600 hover:bg-emerald-900 rounded-md px-5 text-white w-fit"
+            onClick={() => setOpenDelete(!openDelete())}
+          >
+            YES
+          </button>
+          <button
+            class="bg-red-600 hover:bg-red-900 rounded-md px-5 text-white w-fit"
+            onClick={() => setOpenDelete(!openDelete())}
+          >
+            NO
+          </button>
+        </div>
+      </section>
     </main>
   );
 };
