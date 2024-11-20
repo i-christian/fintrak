996cd93efd981f71253e77a1e8ac51ec57d7e185
christian <111076892+i-christian@users.noreply.github.com>
2024-11-20 22:22:42 +0200

    improve the categories page UI

--- frontend/src/pages/Categories.tsx
+++ frontend/src/pages/Categories.tsx
@@ -5,6 +5,11 @@ const Categories: Component = () => {
     <main>
       <button class="btn">New</button>
       <p>List of categories, with edit and delete options</p>
+      <section class="my-5">
+        <ul>
+          <li>categories name and type</li>
+        </ul>
+      </section>
     </main>
   );
 };
