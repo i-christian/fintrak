1bf378b89186ad15b99fee2a25f2b0838cf6fda7
christian <111076892+i-christian@users.noreply.github.com>
2024-11-20 21:58:08 +0200

    feat: implement the budget table UI
    - Created a table which will be used to display the budget

--- frontend/src/pages/Budget.tsx
+++ frontend/src/pages/Budget.tsx
@@ -3,8 +3,41 @@ import { Component } from "solid-js";
 const Budget: Component = () => {
   return (
     <main>
-      <button class="btn">New </button>
-      <p>View Current budget </p>
+      <button class="btn mb-5">New </button>
+      <table class="table-auto min-w-full border border-black rounded-lg shadow-md">
+        <caption class="caption-top font-bold mb-2">
+          Current Budget Name
+        </caption>
+        <thead>
+          <tr>
+            <th class="px-6 py-3 border-b-2 border-gray-500 text-left leading-4 text-blue-500 tracking-wider">
+              Amount
+            </th>
+            <th class="px-6 py-3 border-b-2 text-left leading-4 text-blue-500 border-gray-500 tracking-wider">
+              Category
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+              100
+            </td>
+            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+              Airtime Money{" "}
+            </td>
+          </tr>
+          <tr>
+            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+              1000
+            </td>
+            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+              Transport{" "}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
       <p>List previous budgets </p>
     </main>
   );
