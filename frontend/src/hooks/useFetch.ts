ca10cd232abb203eb0823547acd1a17253ba9af2
christian <111076892+i-christian@users.noreply.github.com>
2024-12-23 12:32:57 +0200

    Add: improve useFetch hook to handle all the necessary requests to the server with various functions

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -8,4 +8,215 @@ export const getUser = async () => {
   if (!response.ok) throw new Error("failed to load user information");
 
   return response.json();
-}
+};
+
+export const getCategories = async () => {
+  const response = await fetch(`//${window.location.host}/api/categories`, {
+    method: "GET",
+    credentials: "include",
+    mode: "cors",
+  });
+
+  if (!response.ok) throw new Error("failed to load user information");
+
+  return response.json();
+};
+
+export const createCategory = async (name: string, type: string) => {
+  const response = await fetch(`//${window.location.host}/api/categories`, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      transaction_type: type,
+    }),
+  });
+
+  if (!response.ok) {
+    const error = await response.json().catch(() => ({}));
+    throw new Error(error.message || "Failed to update category");
+  }
+
+  return "Category created successfully";
+};
+
+export const updateCategory = async (
+  id: string,
+  name: string | null,
+  type: string | null,
+) => {
+  const response = await fetch(
+    `//${window.location.host}/api/categories/${id}`,
+    {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        transaction_type: type,
+      }),
+    },
+  );
+
+  if (!response.ok) {
+    const error = await response.json().catch(() => ({}));
+    throw new Error(error.message || "Failed to create category");
+  }
+
+  return "Category updated successfully";
+};
+
+export const deleteCategory = async (id: String) => {
+  const response = await fetch(
+    `//${window.location.host}/api/categories/${id}`,
+    {
+      method: "DELETE",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  if (!response.ok) {
+    const error = await response.json().catch(() => ({}));
+    throw new Error(error.message || "Failed to update category");
+  }
+
+  return "Category updated successfully";
+};
+
+export const createTransaction = async (
+  category: string,
+  amount: number,
+  notes: string | null,
+) => {
+  const response = await fetch(`//${window.location.host}/api/transactions`, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      category_name: category,
+      amount: amount,
+      notes: notes,
+    }),
+  });
+
+  if (!response.ok) {
+    const error = await response.json().catch(() => ({}));
+    throw new Error(error.message || "Failed to create transaction");
+  }
+
+  return "Transaction created successfully";
+};
+
+export const editTransaction = async (
+  id: string,
+  category: string | null,
+  type: string | null,
+  amount: number | null,
+  notes: string | null,
+) => {
+  const response = await fetch(
+    `//${window.location.host}/api/transactions/${id}`,
+    {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        category_name: category,
+        transaction_type: type,
+        amount: amount,
+        notes: notes,
+      }),
+    },
+  );
+
+  if (!response.ok) {
+    const error = await response.json().catch(() => ({}));
+    throw new Error(error.message || "Failed to update transaction");
+  }
+
+  return "Transaction updated successfully";
+};
+
+export const deleteTransaction = async (id: string) => {
+  const response = await fetch(
+    `//${window.location.host}/api/transactions/${id}`,
+    {
+      method: "DELETE",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  if (!response.ok) {
+    const error = await response.json().catch(() => ({}));
+    throw new Error(error.message || "Failed to delete transaction");
+  }
+
+  return "Transaction deleted successfully";
+};
+
+export const getTotals = async () => {
+  const response = await fetch(
+    `//${window.location.host}/api/transactions/totals`,
+    {
+      method: "GET",
+      credentials: "include",
+      mode: "cors",
+    },
+  );
+
+  if (!response.ok) throw new Error("failed to load user information");
+
+  return response.json();
+};
+
+export const getTransactions = async () => {
+  const response = await fetch(`//${window.location.host}/api/transactions`, {
+    method: "GET",
+    credentials: "include",
+    mode: "cors",
+  });
+
+  if (!response.ok) throw new Error("failed to load user information");
+
+  return response.json();
+};
+
+export const getTransactionsByDate = async (year: string, month: string) => {
+  const params = new URLSearchParams({
+    year: year,
+    month: month,
+  });
+  const url = new URL(`//${window.location.host}/api/transactions/${params}`);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      credentials: "include",
+      mode: "cors",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to load transactions: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching transactions:", error);
+    throw error;
+  }
+};
