11c953f2b55d0ebf542be2646c0f7ba8accccff1
christian <111076892+i-christian@users.noreply.github.com>
2024-12-23 14:21:46 +0200

    feat: improve summary section of transaction page UI
    - Used Solid `Suspense` component and `createResource` to dynamically query data effectively

--- frontend/src/components/Summary.tsx
+++ frontend/src/components/Summary.tsx
@@ -1,45 +1,19 @@
-import { Component, createSignal } from "solid-js";
+import { Accessor, Component } from "solid-js";
 
-const Summary: Component = () => {
-  const [income, setIncome] = createSignal<number>(0);
-  const [expense, setExpense] = createSignal<number>(0);
-
-  const fetchData = async () => {
-    const response = await fetch(
-      `//${window.location.host}/api/transactions/totals`,
-      {
-        method: "GET",
-        credentials: "include",
-        mode: "cors",
-      },
-    );
-
-    if (!response.ok)
-      throw new Error("failed to fetch total income and expenses");
-
-    let data = await response.json();
-
-    const incomeData = data.find(
-      (item: { transaction_type: string; total_amount: number }) =>
-        item.transaction_type === "income",
-    );
-    const expenseData = data.find(
-      (item: { transaction_type: string; total_amount: number }) =>
-        item.transaction_type === "expense",
-    );
-
-    setIncome(incomeData ? incomeData.total_amount : 0);
-    setExpense(expenseData ? expenseData.total_amount : 0);
-  };
-
-  fetchData();
+const Summary: Component<{ total: Accessor<any> }> = (props) => {
+  const income: number | string =
+    props.total()?.find((item: any) => item.transaction_type === "income")
+      ?.total_amount || "N/A";
+  const expense: number | string =
+    props.total()?.find((item: any) => item.transaction_type === "expense")
+      ?.total_amount || "N/A";
 
   return (
     <section class="p-2 border bg-slate-300 rounded-md">
       <header class="text-center font-bold text-2xl">Monthly Summary</header>
       <div class="mt-5 ml-5 text-md">
-        <p class="text-md font-bold text-blue-900">Income: {income()}</p>
-        <p class="text-md font-bold text-red-900">Expense: {expense()}</p>
+        <p class="text-md font-bold text-blue-900">Income: {income}</p>
+        <p class="text-md font-bold text-red-900">Expense: {expense}</p>
       </div>
     </section>
   );
--- frontend/src/pages/Transactions.tsx
+++ frontend/src/pages/Transactions.tsx
@@ -1,12 +1,29 @@
-import { Component } from "solid-js";
+import { Component, createResource, Match, Suspense, Switch } from "solid-js";
+import { getTotals } from "../hooks/useFetch";
 import Summary from "../components/Summary";
 
 const Transactions: Component = () => {
+  const [total, { refetch }] = createResource(getTotals);
+
   return (
     <main>
-      <Summary />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Match when={total.state === "errored"}>
+            <span class="text-red-500 text-xl bg-slate-200 p-4 m-2">
+              Error loading data
+            </span>
+          </Match>
+          <Match when={total.state === "ready"}>
+            <Summary total={total} />
+          </Match>
+        </Switch>
+      </Suspense>
+
       <div class="flex gap-2 flex-wrap sm:flex-nowrap sm:gap-5">
-        <button class="btn">New</button>
+        <button class="btn" onClick={() => refetch()}>
+          New
+        </button>
         <button class="btn">Filter</button>
       </div>
       <p class="mt-5 py-2 border-t-2 border-black text-center text-xl">
